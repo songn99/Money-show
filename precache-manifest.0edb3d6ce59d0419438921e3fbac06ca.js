dd036351e77e78a57b831fae7673175b546bd72e
songn99 <sn429@outlook.com>
2022-10-15 00:56:46 +0800

    deploy

---
+++ precache-manifest.0edb3d6ce59d0419438921e3fbac06ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b733e44b3e06d06fa6",
+    "url": "/Money-show/css/app.12ca2015.css"
+  },
+  {
+    "revision": "8e81f3be0a3d99ee85b8a843d137edb4",
+    "url": "/Money-show/index.html"
+  },
+  {
+    "revision": "51b733e44b3e06d06fa6",
+    "url": "/Money-show/js/app.af395612.js"
+  },
+  {
+    "revision": "b8a63fa27e8826f3ac88",
+    "url": "/Money-show/js/chunk-vendors.49abd244.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/Money-show/manifest.json"
+  },
+  {
+    "revision": "08da34d92d3ba309261e177cda3e21a3",
+    "url": "/Money-show/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Money-show/robots.txt"
+  }
+]);
